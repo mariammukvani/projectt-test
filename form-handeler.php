b79d65e1ac7998c44f53676a64a8eb78679f84cf
mariammukvani <mariam.mukvani@geolab.edu.ge>
2021-06-02 01:49:23 +0400

    website

---
+++ form-handeler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$visitor email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form - 'mukvanimariami@gmail.com';
+
+$email_subject ='New form submission'; 
+
+$email_body - "User Name: $name.\n". 
+                "User Email:$visitor_email.\n".
+                "subject: $subject.\n".
+                "User message: $message.\n";
+
+$to = 'mukvanimariami@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To : $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: contact.html");
+
+
+?>
